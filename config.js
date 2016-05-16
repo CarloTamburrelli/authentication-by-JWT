990f919c7b56b59d40989bdd04baa41ee1040210
Eleclare <kerokaznu2@gmail.com>
2016-05-16 23:47:12 +0200

    initialization

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    'public_key' : "-----BEGIN PUBLIC KEY-----MFswDQYJKoZIhvcNAQEBBQADSgAwRwJAaDzF3phQKduOtm42+RI37yhPwdD8KXaXET0wpqp3sOpOYxkmnK5HYAxej51yJHu/n1iKqi2HZD0ZpN+SmYoKrQIDAQAB-----END PUBLIC KEY-----",
+    'private_key': "-----BEGIN RSA PRIVATE KEY-----MIIBOAIBAAJAaDzF3phQKduOtm42+RI37yhPwdD8KXaXET0wpqp3sOpOYxkmnK5HYAxej51yJHu/n1iKqi2HZD0ZpN+SmYoKrQIDAQABAkBW1rpplS7DkFw2c19s5NagcU7wwvAwgtGb1QRg9Qc95P32LvEfzXqBU+IO+25dYMyjduv3rUhDgnGtwU1hz19pAiEAzanSv1vglrFWjlQgq48QcxAKTnhQ3dlX3RI2tHZO59sCIQCBv/AysCHEwZXnebhC18d4w8QiJni+bqWOkeoJdQKCFwIgS834nAfldCXRXYysrg+BZKfY5Kt8QwlFVXrNJx8kXlkCIG7e1WBq0yAvD2oRv5sWiXOvTOyllS2cnMmb87GLE86XAiAfr+dEiyFq9F7Co+Q3hvRyf+8IOVKiTpmVvfjEqpNnxw==-----END RSA PRIVATE KEY-----",
+    'database': 'mongodb://localhost/foggia'
+
+};
---
+++ server.js
@@ -0,0 +1,149 @@
+// =======================
+var express     = require('express');
+var app         = express();
+var bodyParser  = require('body-parser');
+var morgan      = require('morgan');
+var mongoose    = require('mongoose');
+var fs = require('fs');
+var path = require('path');
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('./config'); // get our config file
+var User   = require('./app/models/user'); // get our mongoose model
+    
+// =======================
+// configuration =========
+// =======================
+var port = process.env.PORT || 8080; // used to create, sign, and verify tokens
+mongoose.connect(config.database); // connect to database
+//app.set('private_key', config.private_key); // private_key
+//app.set('public_key', config.public_key); // public_key
+
+var pub = fs.readFileSync(path.join(__dirname, 'pub.pem'));
+var priv = fs.readFileSync(path.join(__dirname, 'priv.pem'));
+
+// use body parser so we can get info from POST and/or URL parameters
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// use morgan to log requests to the console
+app.use(morgan('dev'));
+
+// =======================
+// routes ================
+// =======================
+// basic route
+var apiRoutes = express.Router(); 
+
+
+
+apiRoutes.get('/authenticate', function(req, res) {
+
+  // find the user
+  User.findOne({
+    name: "Nick Cerminara"
+  }, function(err, user) {
+
+    if (err) throw err;
+
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+
+      // check if password matches
+      if (user.password != "password") {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+
+        // if user is found and password is right
+        // create a token
+        var token = jwt.sign(user, priv, {
+          expiresIn: 60, // expires in 60 seconds
+          algorithm: 'RS256'
+        });
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }   
+
+    }
+
+  });
+});
+
+apiRoutes.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyIkX18iOnsic3RyaWN0TW9kZSI6dHJ1ZSwiZ2V0dGVycyI6e30sIndhc1BvcHVsYXRlZCI6ZmFsc2UsImFjdGl2ZVBhdGhzIjp7InBhdGhzIjp7Il9fdiI6ImluaXQiLCJhZG1pbiI6ImluaXQiLCJwYXNzd29yZCI6ImluaXQiLCJuYW1lIjoiaW5pdCIsIl9pZCI6ImluaXQifSwic3RhdGVzIjp7Imlnbm9yZSI6e30sImRlZmF1bHQiOnt9LCJpbml0Ijp7Il9fdiI6dHJ1ZSwiYWRtaW4iOnRydWUsInBhc3N3b3JkIjp0cnVlLCJuYW1lIjp0cnVlLCJfaWQiOnRydWV9LCJtb2RpZnkiOnt9LCJyZXF1aXJlIjp7fX0sInN0YXRlTmFtZXMiOlsicmVxdWlyZSIsIm1vZGlmeSIsImluaXQiLCJkZWZhdWx0IiwiaWdub3JlIl19LCJlbWl0dGVyIjp7ImRvbWFpbiI6bnVsbCwiX2V2ZW50cyI6e30sIl9ldmVudHNDb3VudCI6MCwiX21heExpc3RlbmVycyI6MH19LCJpc05ldyI6ZmFsc2UsIl9kb2MiOnsiX192IjowLCJhZG1pbiI6dHJ1ZSwicGFzc3dvcmQiOiJwYXNzd29yZCIsIm5hbWUiOiJOaWNrIENlcm1pbmFyYSIsIl9pZCI6IjU3MzhlNTMwMTE1OTY4ZTQxZTYzZGNkYyJ9LCJfcHJlcyI6eyIkX19vcmlnaW5hbF9zYXZlIjpbbnVsbCxudWxsXX0sIl9wb3N0cyI6eyIkX19vcmlnaW5hbF9zYXZlIjpbXX0sImlhdCI6MTQ2MzQwMzg0MCwiZXhwIjoxNDYzNDAzOTAwfQ.5FhJPsaYI99Q5ljyHXaazkM3X6jExzeqhDH3L3-X58I";
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, pub, function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded; 
+          console.log("--->"+JSON.stringify(decoded)+"<---");
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
+    
+  }
+});
+
+// route to show a random message (GET http://localhost:8080/api/)
+apiRoutes.get('/', function(req, res) {
+  res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+// route to return all users (GET http://localhost:8080/api/users)
+apiRoutes.get('/users', function(req, res) {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+});   
+
+// apply the routes to our application with the prefix /api
+app.use('/api', apiRoutes);
+
+app.get('/setup', function(req, res) {
+
+  // create a sample user
+  var nick = new User({ 
+    name: 'Nick Cerminara', 
+    password: 'password',
+    admin: true 
+  });
+
+  // save the sample user
+  nick.save(function(err) {
+    if (err) throw err;
+
+    console.log('User saved successfully');
+    res.json({ success: true });
+  });
+});
+
+// API ROUTES -------------------
+// we'll get to these in a second
+
+// =======================
+// start the server ======
+// =======================
+app.listen(port);
+console.log('Magic happens at http://localhost:' + port);
